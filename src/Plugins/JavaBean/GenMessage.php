64f3a890abff2620769e0d32725b41802b22981f
ewenlaz <ewenlaz@ewenlazdeMacBook-Pro.local>
2018-09-12 17:05:11 +0800

    add javabean builder

--- src/Plugins/JavaBean/GenMessage.php
+++ src/Plugins/JavaBean/GenMessage.php
@@ -30,9 +30,60 @@ class GenMessage extends GenMessageBase
             ucfirst($this->message->getName())
         ));
         foreach ($this->message->getField() as $filed) {
+            $this->pushLine('');
+            $this->writeDoc($filed, 1);
             $this->pushLine($this->genField($filed), 1);
         }
+        $this->pushLine('');
+        $this->pushLine('public static class Builder {', 1);
+        foreach ($this->message->getField() as $filed) {
+            $this->pushLine($this->genField($filed, 'private'), 2);
+        }
+        $this->pushLine('public Builder() {', 2);
+        $this->pushLine('}', 2);
+
+        foreach ($this->message->getField() as $filed) {
+            $this->pushFieldBuildMethod($filed);
+        }
+
+        $this->pushLine(sprintf(
+            'public %s build() {',
+            ucfirst($this->message->getName())
+        ), 2);
+        $this->pushLine(sprintf(
+            '%s c = new %s();',
+            ucfirst($this->message->getName()),
+            ucfirst($this->message->getName())
+        ), 3);
+        foreach ($this->message->getField() as $filed) {
+            $this->pushLine(sprintf(
+                'c.%s = %s;',
+                $filed->getName(),
+                $filed->getName()
+            ), 3);
+        }
+        $this->pushLine('return c;', 3);
+        $this->pushLine('}', 2);
+        $this->pushLine('}', 1);
         $this->pushLine('}');
+    }
+
+    protected function pushFieldBuildMethod(FieldDescriptorProto $field)
+    {
+        $javaType = $this->getFieldType($field);
+        $this->writeDoc($field, 2);
+        $this->pushLine(sprintf(
+            'public Builder set%s(%s) {',
+            ucfirst($field->getName()),
+            $this->genFieldParam($field)
+        ), 2);
+        $this->pushLine(sprintf(
+            'this.%s = %s;',
+            $field->getName(),
+            $field->getName()
+        ), 3);
+        $this->pushLine('return this;', 3);
+        $this->pushLine('}', 2);
     }
 
     protected function mapNested(DescriptorProto $nested)
@@ -43,26 +94,50 @@ class GenMessage extends GenMessageBase
         array_push($this->mapEntrys, $nested->getName());
     }
 
-    protected function genField(FieldDescriptorProto $field)
+    protected function genFieldParam(FieldDescriptorProto $field)
     {
         $javaType = $this->getFieldType($field);
-        $this->writeDoc($field, 1);
         if ($field->getLabel() === FiledLable::LABEL_REPEATED) {
             //is map field
             if ($this->isMapFiled($field)) {
                 $keyTypeName = $this->getFieldType($this->getMapKeyField($field));
                 $valueTypeName = $this->getFieldType($this->getMapValueField($field));
                 return sprintf(
-                    'public Map<%s, %s> %s;',
+                    'Map<%s, %s> %s',
+                    $decorate,
                     $keyTypeName,
                     $valueTypeName,
                     $field->getName()
                 );
             } else {
-                return sprintf('public List<%s> %s;', $javaType, $field->getName());
+                return sprintf('List<%s> %s', $javaType, $field->getName());
             }
         } else {
-            return sprintf('public %s %s;', $javaType, $field->getName());
+            return sprintf('%s %s', $javaType, $field->getName());
+        }
+    }
+
+
+    protected function genField(FieldDescriptorProto $field, $decorate = 'public')
+    {
+        $javaType = $this->getFieldType($field);
+        if ($field->getLabel() === FiledLable::LABEL_REPEATED) {
+            //is map field
+            if ($this->isMapFiled($field)) {
+                $keyTypeName = $this->getFieldType($this->getMapKeyField($field));
+                $valueTypeName = $this->getFieldType($this->getMapValueField($field));
+                return sprintf(
+                    '%s Map<%s, %s> %s;',
+                    $decorate,
+                    $keyTypeName,
+                    $valueTypeName,
+                    $field->getName()
+                );
+            } else {
+                return sprintf('%s List<%s> %s;', $decorate, $javaType, $field->getName());
+            }
+        } else {
+            return sprintf('%s %s %s;', $decorate, $javaType, $field->getName());
         }
     }
 
